f6588527d0b255845d14aebe319572c1d80840bc
lgc-s <luisguilhermecs3@gmail.com>
2025-07-10 19:20:46 -0300

    app updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css'
 
-import { Button } from './components/Button/Button';
 import { Content } from './components/Content/Content';
 import { DataReader } from './components/DataReader/DataReader';
 import { Header } from './components/Header/Header';
@@ -13,9 +12,8 @@ function App() {
         <main>
             <Content>
                 <Text type="Title">Welcome to <b>Vistadoro</b></Text>
-                <Text type="Normal">Vistadoro is an app able to receive a .js data file and turn it into a fully visible table.</Text>
-                <Text type="Normal">There are some options available to use. on clicking on one of them, the table will be loaded</Text>
-                <Button>Table</Button>
+                <Text type="Normal">Vistadoro is an app able to receive a .js data file and turn it into a basic table.</Text>
+                <Text type="Normal">There are some theme options available to use. on clicking on one of them, the site design will be changed</Text>
                 <DataReader />
             </Content>
         </main>
---
+++ src/data/data.js
@@ -0,0 +1,11 @@
+export const data = [
+    ["#", "Color"      , "Code"        ],
+    ["1", "Pure Red"   , "rgb[255,000,000]"],
+    ["2", "Pure Green" , "rgb[000,252,000]"],
+    ["3", "Pure Blue"  , "rgb[000,000,255]"],
+    ["4", "Pure Orange", "rgb[255,100,000]"],
+    ["5", "Gray"       , "rgb[031,031,031]"],
+    ["6", "Deep Blue"  , "rgb[031,031,127]"],
+    ["7", "Black"      , "rgb[000,000,000]"],
+    ["8", "White"      , "rgb[255,255,255]"],
+];
