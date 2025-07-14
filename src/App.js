2759fb47b1ecff8f665d5c1b039f437b0d0f7658
lgc-s <luisguilhermecs3@gmail.com>
2025-07-14 18:00:50 -0300

    app updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css'
 
+import { Button } from './components/Button/Button';
 import { Content } from './components/Content/Content';
 import { DataReader } from './components/DataReader/DataReader';
 import { Header } from './components/Header/Header';
@@ -11,12 +12,20 @@ function App() {
     <div className="App">
         <Header/>
         <main>
-            <Content>
-                <Text type="Title">Welcome to <b>Vistadoro</b></Text>
-                <Text type="Normal">Vistadoro is an <b>ReactJS App</b> able to receive a .js data file and turn it into a basic table.</Text>
-                <Text type="Normal">There are some theme options available to use. on clicking on one of them, the site design will be <b>changed</b>.</Text>
-                <DataReader />
-            </Content>
+          <Content>
+            <Text type="Title">Welcome to <b>Vistadoro</b></Text>
+            <Text type="Normal">Vistadoro is an <b>ReactJS App</b> able to receive a .js data file and turn it into a basic table.</Text>
+            <Text type="Normal">There are some theme options available to use. on clicking on one of them, the site design will be <b>changed</b>.</Text>
+            <div className='button-menu'>
+              <Button type="Theme 1">BASE</Button>
+              <Button type="Theme 2">DARK</Button>
+              <Button type="Theme 3">LAVA</Button>
+              <Button type="Theme 4">BLUE</Button>
+              <Button type="Theme 5">PINK</Button>
+              <Button type="Theme 6">XTRA</Button>
+            </div>
+            <DataReader />
+          </Content>
         </main>
         <Text type="Footer">Made by <Link address="https://www.linkedin.com/in/lgc-s/">LGC-S</Link></Text>
     </div>
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,11 +1,41 @@
-import { Text } from '../Text/Text'
-
 import "./Button.css"
 
 export function Button(props) {
-    return (
-        <Text type="Button">
-            {props.children}
-        </Text>
-    )
+    if (props.type === "Theme 1") {
+        return (
+            <div className='button theme1'>
+                   {props.children}      
+            </div>
+        )
+    } else if (props.type === "Theme 2") {
+        return (
+            <div className='button theme2'>
+                {props.children}
+            </div>
+        )
+    } else if (props.type === "Theme 3") {
+        return (
+            <div className='button theme3'>
+                {props.children}
+            </div>
+        )
+    } else if (props.type === "Theme 4") {
+        return (
+            <div className='button theme4'>
+                {props.children}
+            </div>
+        )
+    } else if (props.type === "Theme 5") {
+        return (
+            <div className='button theme5'>
+                {props.children}
+            </div>
+        )
+    } else if (props.type === "Theme 6") {
+        return (
+            <div className='button theme6'>
+                {props.children}
+            </div>
+        )
+    }
 }
