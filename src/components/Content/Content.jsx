b77a69f62c2c1fa749f100b3eb38f7788d054969
lgc-s <luisguilhermecs3@gmail.com>
2025-07-18 00:58:23 -0300

    mobile notice added

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,7 +1,16 @@
 import "./Content.css"
 
+import { Text } from '../Text/Text'
+
 export function Content(props) {
     return (
-        <div className="content">{props.children}</div>
+        <div>
+            <div className="content">{props.children}</div>
+            <div className="content-mobile">
+                <Text type="Title">Mobile <b>not</b> supported</Text>
+                <Text type="Normal">Vistadoro is not intended to be used on mobile devices</Text>
+            </div>
+        </div>
+
     )
 }
