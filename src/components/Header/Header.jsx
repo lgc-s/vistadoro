01da76d5cec9e6fa3b9957770023c37c25c441da
lgc-s <luisguilhermecs3@gmail.com>
2025-07-18 00:49:45 -0300

    themes updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,6 +6,9 @@ import { ThemeContext } from "../Theme/ThemeContext";
 import "./Header.css"
 import logo from "../../image/vistadoro1.png"
 import logo_dark from "../../image/vistadoro2.png"
+import logo_lava from "../../image/vistadoro3.png"
+import logo_blue from "../../image/vistadoro4.png"
+import logo_pink from "../../image/vistadoro5.png"
 
 export function Header(props) {
 
@@ -22,6 +25,34 @@ export function Header(props) {
         return (
             <div className="header">
                 <img src={logo_dark} alt="vistadoro"/>
+                <Text type="Menu"><Link address="https://github.com/lgc-s/vistadoro">Documentation</Link></Text>
+            </div>
+        )
+    } else if (theme === "lava") {
+        return (
+            <div className="header">
+                <img src={logo_lava} alt="vistadoro"/>
+                <Text type="Menu"><Link address="https://github.com/lgc-s/vistadoro">Documentation</Link></Text>
+            </div>
+        )
+    } else if (theme === "blue") {
+        return (
+            <div className="header">
+                <img src={logo_blue} alt="vistadoro"/>
+                <Text type="Menu"><Link address="https://github.com/lgc-s/vistadoro">Documentation</Link></Text>
+            </div>
+        )
+    } else if (theme === "pink") {
+        return (
+            <div className="header">
+                <img src={logo_pink} alt="vistadoro"/>
+                <Text type="Menu"><Link address="https://github.com/lgc-s/vistadoro">Documentation</Link></Text>
+            </div>
+        )
+    } else if (theme === "xtra") {
+        return (
+            <div className="header">
+                <img src={logo} alt="vistadoro"/>
                 <Text type="Menu"><Link address="https://github.com/lgc-s/vistadoro">Documentation</Link></Text>
             </div>
         )
